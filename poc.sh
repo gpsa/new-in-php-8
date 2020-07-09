58733b9ae87808ce26f988e6360b02eaea1bdcac
Guilherme Pereira da Silva Alves <guilhermepsa@gmail.com>
2020-07-09 14:32:15 -0500

    ADD Docker project and poc.sh improvements
    - Added git submodule to build a local image of PHP8
    - Added `build` command to run `docker-compose build` easily.

--- poc.sh
+++ poc.sh
@@ -10,17 +10,27 @@ usage ()
         scripts="${scripts} $(basename $f)"
     done
 
-    echo " Usage: $0 7|8 script
- Options:
+    echo " Usage: $0 [options] (command)
+ options:
      7 RUN ON PHP 7
      8 RUN ON PHP 8, default
- Available scripts:
- [ ${scripts} ]"
-
+ command:
+    build:
+        - Builds a php image for the PoC. Only works for PHP8
+    help:
+        - Displays this help
+    *.php:
+        - Run any PHP for PoC.
+        - AVAILABLE SCRIPTS:
+        ${scripts}
+    "
 }
 
 test "$1" == "help"
 IS_HELP=$?
+
+test "$1" == "build"
+IS_BUILD=$?
 
 if [[ $IS_HELP -ne 0 && $# -eq 1 ]]
 then
@@ -35,12 +45,12 @@ else
     exit;
 fi
 
-# function run_on_docker(){
-#
-# }
+function run_docker_compose_build(){
+    docker-compose build "php${VERSION}"
+    # > /dev/null
+}
 
 function run_on_docker_compose(){
-    docker-compose build "php${VERSION}" > /dev/null
     docker-compose run --rm "php${VERSION}" -d error_reporting=E_ALL -d display_errors=1 ${SCRIPT}
 }
 # echo $POC_DIR $VERSION $SCRIPT
@@ -50,7 +60,12 @@ HAS_DOCKER_COMPOSE=$(which docker-compoxse)
 
 if [[ $HAS_DOCKER_COMPOSE -eq 0 ]]
 then
-    cd ${POC_DIR} && run_on_docker_compose $VERSION $SCRIPT
+    if [[ $IS_BUILD -eq 0 ]]
+    then
+        cd ${POC_DIR} && run_docker_compose_build $VERSION
+    else
+        cd ${POC_DIR} && run_on_docker_compose $VERSION $SCRIPT
+    fi
 else
     echo "Docker Compose not found"
     echo "This script is not ready for running on Docker only"
